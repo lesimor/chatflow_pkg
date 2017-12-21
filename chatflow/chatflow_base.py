d465fdc099204087fe181f4c9686a1636dc7ae97
lesimor <brillante91@naver.com>
2017-12-21 15:27:38 +0900

    kakao 템플릿 지원

--- chatflow/chatflow_base.py
+++ chatflow/chatflow_base.py
@@ -8,7 +8,7 @@ class ChatFlowBase(object):
 
     def post(self, user_key, message):
         r = requests.post('http://chatflow.kr/api/v1/message',
-                          data={'user_key': user_key,
+                          data={'user_id': user_key,
                                 'content': message,
                                 'developer_id': self.developer_id,
                                 'conversation_id': self.conversation_id})
--- chatflow/kakao.py
+++ chatflow/kakao.py
@@ -4,4 +4,32 @@ from chatflow.chatflow_base import ChatFlowBase
 class Kakao(ChatFlowBase):
     def __init__(self, dev_id, conv_id):
         ChatFlowBase.__init__(self, dev_id, conv_id)
+        self.template = {
+            "message": {
+                "text": "",
+                "photo": None,
+                "message_button": None
+            },
+            "keyboard": {
+                "type": "text",
+                "buttons": None
+            }
+        }
 
+    def response(self, user_key, message):
+        r = self.post(user_key, message)
+
+        bot_message = r['message']['text']
+        bot_image_url = r['message']['image']
+
+        is_button = r['keyboard']['type'] == 'buttons'
+
+        return {
+            "message": {
+                "text": bot_message,
+                "photo": None,
+                "message_button": None
+            },
+            "keyboard": None if r['keyboard']['type'] == 'text'
+                        else {"type": "buttons", "buttons": r['keyboard']['buttons']}
+        }
