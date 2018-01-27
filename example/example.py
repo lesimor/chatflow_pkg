c8686e47db042d6cab6b1185934c339c96e1ac4e
lesimor <brillante91@naver.com>
2018-01-27 16:29:30 +0900

    Change to run examples from the root directory

--- example/example.py
+++ example/example.py
@@ -1,15 +1,23 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+abs_path = os.path.dirname(os.path.abspath(__file__))
+
 from chatflow import ChatFlow
 
-cf = ChatFlow('./state.json')
+file_name = 'state.json'
+cf = ChatFlow(os.path.join(abs_path, file_name))
+
 
 def main():
     msg = None
 
     while msg != 'q':
-        msg = input("사용자: ")
+        msg = input("User: ")
         next_state = cf.run(cf.entry_id, msg)  # current state를 임의로 entry로 설정
 
-        print("Bot: {}".format(next_state.get('message')))
+        print("System: {}".format(next_state.get('message')))
 
 if __name__ == '__main__':
     main()
