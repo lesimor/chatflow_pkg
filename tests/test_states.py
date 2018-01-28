2690ac3f9aef646b0ef8776d48b4b81cfd1453a2
wonyoHwang <hollal0726@gmail.com>
2018-01-28 20:16:57 +0900

    cover test basic state class

---
+++ tests/test_states.py
@@ -0,0 +1,14 @@
+import unittest
+
+from chatflow.states import State
+
+class TestStates(unittest.TestCase):
+    def test_state(self):
+        json_dict = {
+            "name": "테스트 노드",
+            "message": "테스트 노드입니다."
+        }
+        state = State('test_id', **json_dict)
+        self.assertEqual(state.id, "test_id")
+        self.assertEqual(state.name, "테스트 노드")
+        self.assertEqual(state.message, "테스트 노드입니다.")
